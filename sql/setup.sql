374f54f144e8d6f798ba836f3c0ed39295a06d2a
Tyler Watson <tylerww91@gmail.com>
2022-11-09 17:45:46 -0800

    adds pets data table, setup database

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,21 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS pets;
+
+CREATE TABLE pets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL
+);
+
+INSERT INTO pets(
+    name,
+    type
+)
+
+VALUES
+('Sevro', 'cat'),
+('Eevee', 'dog'),
+('Maple', 'cat'),
+('Doctor', 'cat'),
+('Beans', 'cat');
