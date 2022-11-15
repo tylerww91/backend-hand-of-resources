f31b20fafc7f0a507ff4f24bf9fc6cc793ee34b7
Tyler Watson <tylerww91@gmail.com>
2022-11-14 16:54:16 -0800

    builds GET /players route and test

--- __tests__/players.test.js
+++ __tests__/players.test.js
@@ -11,5 +11,57 @@ describe('players routes', () => {
   it('GET /players should return a list of players', async () => {
     const resp = await request(app).get('/players');
     expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "name": "Stephen Curry",
+          "position": "PG",
+          "team": "Warriors",
+        },
+        Object {
+          "id": "2",
+          "name": "Ja Morant",
+          "position": "PG",
+          "team": "Grizzlies",
+        },
+        Object {
+          "id": "3",
+          "name": "Evan Mobley",
+          "position": "PF",
+          "team": "Caveliers",
+        },
+        Object {
+          "id": "4",
+          "name": "Damian Lillard",
+          "position": "PG",
+          "team": "Blazers",
+        },
+        Object {
+          "id": "5",
+          "name": "Luka Doncic",
+          "position": "PG",
+          "team": "Mavericks",
+        },
+        Object {
+          "id": "6",
+          "name": "Joel Embiid",
+          "position": "C",
+          "team": "76ers",
+        },
+        Object {
+          "id": "7",
+          "name": "Lebron James",
+          "position": "PF",
+          "team": "Lakers",
+        },
+        Object {
+          "id": "8",
+          "name": "Kawhi Leonard",
+          "position": "SF",
+          "team": "Clippers",
+        },
+      ]
+    `);
   });
 });
